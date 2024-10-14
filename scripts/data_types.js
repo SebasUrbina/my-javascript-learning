3aae9c190157ce00833496cd6f8601ebd161ff3c
SebasUrbina <sebastian.urbina@live.cl>
2024-10-13 23:10:33 -0300

    first commit

---
+++ scripts/data_types.js
@@ -0,0 +1,17 @@
+//number
+1997
+
+//string
+"Hello World"
+
+//bool
+True
+False
+
+//Null javascript have two types of nulls
+Null //The absence of value.
+Undefined //A variable not yet assigned a value.
+
+//ES6 version introduced the 
+//BigInt
+//Symbol
---
+++ scripts/my_first.js
@@ -0,0 +1,20 @@
+// this is a comment!
+
+/*
+This 
+is
+a
+multi-line
+comment
+*/
+
+console.log("hello world")
+
+console.log("%cHello, World", "color: blue; font-size: 40px");
+
+var person;
+var person = "John"
+console.log("Hello", person)
+
+var greeting = "Hello"
+console.log(greeting, person)
